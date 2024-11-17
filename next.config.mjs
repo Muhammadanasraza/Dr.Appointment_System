8aa6ec6d1dc4ba584e701559f66ce036cbf21be3
Anas raza <154615716+Muhammadanasraza@users.noreply.github.com>
2024-11-17 15:59:02 +0500

    add

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
         remotePatterns : [
             {
                 hostname : "img.freepik.com"
+            },
+            {
+                hostname : "lh3.googleusercontent.com"
             }
         ]
     }
